af823c87d2bb3d979b9a986136d620c04527481f
anany <ananyevpri@gmail.com>
2026-04-09 23:11:04 +0300

    Add initial implementation of CNN project with ConvolutionLayer and project setup

---
+++ Autograd.CNN/CNN.cs
@@ -0,0 +1,8 @@
+﻿using Autograd.Engine.Core;
+
+namespace Autograd.CNN;
+
+public class CNN
+{
+
+}
---
+++ Autograd.CNN/ConvolutionLayer.cs
@@ -0,0 +1,29 @@
+﻿using Autograd.Engine.Core;
+
+namespace Autograd.CNN;
+
+public class ConvolutionLayer
+{
+    private readonly Tensor _kernel;
+    private readonly Tensor _b;
+
+    public Tensor Forward(Tensor input) => Tensor.Convolution(input, _kernel) + _b;
+
+    /// <summary>
+    /// Zero out gradients
+    /// </summary>
+    public void Zero()
+    {
+        _kernel.Zero();
+        _b.Zero();
+    }
+
+    /// <summary>
+    /// Adjust weights and biases according to gradients
+    /// </summary>
+    public void Adjust(float rate)
+    {
+        _kernel.Adjust(rate);
+        _b.Adjust(rate);
+    }
+}
