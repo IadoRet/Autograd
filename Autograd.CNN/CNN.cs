284ffdd58e718cc5efce297c7c482cf44869f249
anany <ananyevpri@gmail.com>
2026-04-16 20:44:47 +0300

    Add support for activation functions and layer chaining in CNN implementation

--- Autograd.CNN/CNN.cs
+++ Autograd.CNN/CNN.cs
@@ -1,8 +1,60 @@
-﻿using Autograd.Engine.Core;
+using Autograd.Engine.Core;
+using Autograd.Engine.Enums;
 
 namespace Autograd.CNN;
 
+/// <summary>
+/// Convolutional neural network
+/// </summary>
+// ReSharper disable once InconsistentNaming
 public class CNN
 {
+    private readonly int _channels;
+    private LinkedList<ConvolutionLayer> _layers;
+    private Random _random = new Random();
 
+    private CNN(int channels)
+    {
+        _channels = channels;
+        _layers = [];
+    }
+
+    public static CNN Create(int channels)
+    {
+        return new CNN(channels);
+    }
+
+    public CNN WithLayer(int kernelSize, ActivationType activation)
+    {
+        _layers.AddLast(new ConvolutionLayer(_channels, kernelSize, _random, activation));
+
+        return this;
+    }
+
+    public CNN WithOutput(int kernelSize)
+    {
+        _layers.AddLast(new ConvolutionLayer(_channels, kernelSize, _random));
+
+        return this;
+    }
+
+    public Tensor Forward(Tensor input)
+    {
+        foreach (ConvolutionLayer layer in _layers)
+            input = layer.Forward(input);
+
+        return input;
+    }
+
+    public void Zero()
+    {
+        foreach (ConvolutionLayer layer in _layers)
+            layer.Zero();
+    }
+
+    public void Adjust(float learningRate)
+    {
+        foreach (ConvolutionLayer layer in _layers)
+            layer.Adjust(learningRate);
+    }
 }
--- Autograd.CNN/ConvolutionLayer.cs
+++ Autograd.CNN/ConvolutionLayer.cs
@@ -1,13 +1,57 @@
 ﻿using Autograd.Engine.Core;
+using Autograd.Engine.Enums;
 
 namespace Autograd.CNN;
 
+/// <summary>
+/// Convolution layer
+/// </summary>
 public class ConvolutionLayer
 {
+    /// <summary>
+    /// Activation function
+    /// </summary>
+    private readonly ActivationType? _activation;
+
+    /// <summary>
+    /// Kernel (filters)
+    /// </summary>
     private readonly Tensor _kernel;
+
+    /// <summary>
+    /// Biases
+    /// </summary>
     private readonly Tensor _b;
 
-    public Tensor Forward(Tensor input) => Tensor.Convolution(input, _kernel) + _b;
+    public ConvolutionLayer(int channels, int kernelSize, Random random, ActivationType? activation = null)
+    {
+        _activation = activation;
+
+        int size = channels * kernelSize * kernelSize;
+        float[] kernelData = new float[size];
+
+        for (int i = 0; i < size; i++)
+            kernelData[i] = random.NextSingle() * 2 - 1;
+
+        // kernel shape: [1, channels, kernelSize, kernelSize]
+        _kernel = new Tensor(kernelData, [1, channels, kernelSize, kernelSize]);
+        _b = new Tensor(new float[channels], [1, channels, 1, 1]);
+    }
+
+    /// <summary>
+    /// Forward pass
+    /// </summary>
+    public Tensor Forward(Tensor input)
+    {
+        Tensor pass = Tensor.Convolution(input, _kernel) + _b;
+
+        return _activation switch
+        {
+            ActivationType.ReLU => Tensor.ReLU(pass),
+            ActivationType.Tanh => Tensor.TanH(pass),
+            _ => pass
+        };
+    }
 
     /// <summary>
     /// Zero out gradients
